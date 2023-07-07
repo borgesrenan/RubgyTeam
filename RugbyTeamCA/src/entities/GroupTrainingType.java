37147f2eb44e38b229544476ed53ac16e009ea9f
Renan Borges <51171566+borgesrenan@users.noreply.github.com>
2023-07-07 23:45:53 +0100

    Created a enum class  to specify the type of group training for a team

---
+++ RugbyTeamCA/src/entities/GroupTrainingType.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entities;
+
+/**
+ *
+ * @author renan
+ */
+public enum GroupTrainingType {
+    TEAM_1, TEAM_2, TEAM_3, TEAM_4, TEAM_5, TEAM_6, TEAM_7, TEAM_8, TEAM_9, 
+    TEAM_10, TEAM_11,
+}
